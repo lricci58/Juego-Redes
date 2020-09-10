cb982ce865cec025629352420f17103c02e6ae3f
ricci58Lucas <lucric2001@gmail.com>
2020-09-09 23:57:17 -0300

    se actualizo la importacion de animacion de soldado1

---
+++ Assets/Codigo/UnidadHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnidadHandler : MonoBehaviour 
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("right")){
+            gameObject.transform.Translate(200f * Time.deltaTime, 0, 0);
+        }
+    }
+}
