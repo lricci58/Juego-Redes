cc02f998c06df42356522e5a2b8909472e4f81d6
ricci58Lucas <lucric2001@gmail.com>
2020-09-11 03:48:59 -0300

    se soluciono el problema del merge, se actualizo el proyecto a la version 1.5f de unity

---
+++ Assets/Codigo/MapaManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapaManager : MonoBehaviour
+{
+    // Esta clase es para setear cada mapa (el mapa que se usa, las colisiones, zonas de despliegue, etc)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Codigo/UnidadManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnidadManager : MonoBehaviour 
+{
+    bool seleccionado = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseClick();
+
+        // permite mover a la unidad
+        if (seleccionado)
+        {
+            // TODO: deberia dejar mover de tile en tile en un rango determinado
+
+            if (Input.GetKey("right"))
+            {
+                gameObject.transform.Translate(200f * Time.deltaTime, 0, 0);
+                gameObject.GetComponent<Animator>().SetBool("moviendo", true);
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            }
+            if (Input.GetKey("left"))
+            {
+                gameObject.transform.Translate(-200f * Time.deltaTime, 0, 0);
+                gameObject.GetComponent<Animator>().SetBool("moviendo", true);
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
+            if (Input.GetKey("up"))
+            {
+                gameObject.transform.Translate(0, 200f * Time.deltaTime, 0);
+                gameObject.GetComponent<Animator>().SetBool("moviendo", true);
+            }
+            if (Input.GetKey("down"))
+            {
+                gameObject.transform.Translate(0, -200f * Time.deltaTime, 0);
+                gameObject.GetComponent<Animator>().SetBool("moviendo", true);
+            }
+
+            if (!Input.GetKey("right") && !Input.GetKey("left") && !Input.GetKey("up") && !Input.GetKey("down"))
+            {
+                gameObject.GetComponent<Animator>().SetBool("moviendo", false);
+            }
+        }
+    }
+
+    void MouseClick()
+    {
+        // detecta si hizo click
+        if (Input.GetMouseButtonDown(0))
+        {
+            // detecta si se hizo click dentro del collider
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+            if (hit)
+            {
+                seleccionado = !seleccionado;
+            }
+        }
+    }
+}
