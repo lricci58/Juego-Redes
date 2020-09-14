52ef6aa2fc9f624b81baba4b713ecbfdf59584f0
ricci58Lucas <lucric2001@gmail.com>
2020-09-14 13:09:28 -0300

    se cambio el nombre del controlador del juego

---
+++ Assets/Codigo/ControladorJuego.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class ControladorJuego : MonoBehaviour
+{
+    public static ControladorJuego instancia = null;
+    public ControladorMapa mapa;
+    
+    // en un futuro, la lista de prefabs de unidades se añadiran al mapa, no al juego
+    public Unidad objUnidad;
+    private Grilla grilla;
+
+    private bool unidadSeleccionada;
+
+    void Awake()
+    {
+        if (instancia == null)
+            instancia = this;
+        else if (instancia != this)
+            Destroy(gameObject);
+
+        mapa = GetComponent<ControladorMapa>();
+
+        objUnidad.GetComponent<Unidad>();
+
+        // @TODO: obtener estos parametros del objeto mapa
+        grilla = new Grilla(16, 12, 128f, new Vector3(-128f*8,-128*6));
+
+        Vector3 posMundo = grilla.ObtenerPosMundo(5, 5);
+        objUnidad.SetPosicion(posMundo);
+        
+        IniciarJuego();
+    }
+
+    void IniciarJuego()
+    {
+        mapa.setearEscena();
+    }
+
+    void Update()
+    {
+        unidadSeleccionada = objUnidad.EstaSeleccionada();
+
+        if (unidadSeleccionada)
+            SeleccionarTile();
+    }
+
+    void SeleccionarTile()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            // obtiene la posicion del mouse dentro del juego
+            Vector3 mousePosMundo = Camera.main.ScreenToWorldPoint(mousePos);
+
+            int xTile, yTile;
+            // obtiene
+            bool clickeoEnGrilla = grilla.DetectarClick(mousePosMundo, out xTile, out yTile);
+            
+            if (clickeoEnGrilla)
+            {
+                Vector3 posMundo = grilla.ObtenerPosMundo(xTile, yTile);
+
+                objUnidad.Mover(posMundo);
+            }
+        }
+    }
+}
