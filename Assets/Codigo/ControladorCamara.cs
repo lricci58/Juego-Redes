d536c933511830f3041a48bba31a3e3ac4a493f9
ricci58Lucas <lucric2001@gmail.com>
2020-09-19 22:01:59 -0300

    Negreada
    Se reseto la branch para que este 'ahead of master'

---
+++ Assets/Codigo/ControladorCamara.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+
+public class ControladorCamara : MonoBehaviour
+{
+    // @TODO: revisar el asset RTS Camera, puede que cumpla con todas las features de este script y mas...
+
+    [SerializeField] private float velocidadMovimientoCam = 500f;
+    [SerializeField] private float factorZoom = 400f;
+    [SerializeField] private float velocidadZoom = 100f;
+    [SerializeField] private float limAlto = 210;
+    [SerializeField] private float limAncho = 310;
+
+    private Camera cam;
+    private float altoCamara;
+    private float anchoCamara;
+
+    private float finZoom;
+    private float minZoom = 200f;
+    private float maxZoom = 450f;
+
+    private Vector2 limMapa;
+    private float bordeMapa = 10f;
+
+    void Start()
+    {
+        cam = Camera.main;
+        finZoom = cam.orthographicSize;
+
+        limMapa = new Vector2(limAncho, limAlto);
+    }
+
+    void Update()
+    {
+        MoverTemporal();
+
+        // MoverCamara();
+        ZoomCamara();
+    }
+
+    void MoverCamara()
+    {
+        Vector3 posicionCamara = transform.position;
+
+        // al mantener tecla
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - bordeMapa)
+        {
+            // modifica el vector temporal que contiene la posicion de la camara
+            posicionCamara.y += velocidadMovimientoCam * Time.deltaTime;
+        }
+        else if (Input.GetKey("s") || Input.mousePosition.y <= bordeMapa)
+        {
+            posicionCamara.y -= velocidadMovimientoCam * Time.deltaTime;
+        }
+
+        if (Input.GetKey("a") || Input.mousePosition.x <= bordeMapa)
+        {
+            posicionCamara.x -= velocidadMovimientoCam * Time.deltaTime;
+        }
+        else if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - bordeMapa)
+        {
+            posicionCamara.x += velocidadMovimientoCam * Time.deltaTime;
+        }
+
+        // @TODO: corregir el clamp al hacer zoom, este debe depender directamente del tamaño de la camara
+        posicionCamara.x = Mathf.Clamp(posicionCamara.x, -limMapa.x, limMapa.x);
+        posicionCamara.y = Mathf.Clamp(posicionCamara.y, -limMapa.y, limMapa.y + 100f);
+
+        // actualiza la posicion de la camara
+        transform.position = posicionCamara;
+    }
+
+    void MoverTemporal()
+    {
+        Vector3 posicionCamara = transform.position;
+
+        // al mantener tecla
+        if (Input.GetKey("w"))
+        {
+            // modifica el vector temporal que contiene la posicion de la camara
+            posicionCamara.y += velocidadMovimientoCam * Time.deltaTime;
+        }
+        else if (Input.GetKey("s"))
+        {
+            posicionCamara.y -= velocidadMovimientoCam * Time.deltaTime;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            posicionCamara.x -= velocidadMovimientoCam * Time.deltaTime;
+        }
+        else if (Input.GetKey("d"))
+        {
+            posicionCamara.x += velocidadMovimientoCam * Time.deltaTime;
+        }
+
+        // @TODO: corregir el clamp al hacer zoom, este debe depender directamente del tamaño de la camara
+        posicionCamara.x = Mathf.Clamp(posicionCamara.x, -limMapa.x, limMapa.x);
+        posicionCamara.y = Mathf.Clamp(posicionCamara.y, -limMapa.y, limMapa.y + 150f);
+
+        // actualiza la posicion de la camara
+        transform.position = posicionCamara;
+    }
+
+    void ZoomCamara()
+    {
+        // obtiene la "cantidad de scroll" que se hizo
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        // calcula la cantidad de zoom
+        finZoom -= scroll * factorZoom;
+        // limita el zoom
+        finZoom = Mathf.Clamp(finZoom, minZoom, maxZoom);
+        // ejecuta el movimiento suave
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, finZoom, velocidadZoom * Time.deltaTime);
+
+        // sin uso
+        altoCamara = 2f * cam.orthographicSize;
+        anchoCamara = altoCamara * cam.aspect;
+    }
+}
---
+++ Assets/Codigo/ControladorMapa.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ControladorMapa : MonoBehaviour
+{
+    // almacenan las listas de prefabs
+    [SerializeField] private GameObject[] unidades;
+    [SerializeField] private GameObject[] mapas;
+    [SerializeField] private GameObject[] rios;
+    [SerializeField] private GameObject[] muros;
+
+    [SerializeField] private LayerMask layerColision;
+
+    // se usa para contener a todos los objetos del juego y dejar limpia la hierarchy
+    private Transform contenedorMapa;
+    private Transform contenedorUnidades;
+    private Transform contenedorTiles;
+
+    [SerializeField] private GameObject tileMovimiento;
+    private List<GameObject> listaTilesDeMovimiento;
+
+    [SerializeField] private int ancho;
+    [SerializeField] private int alto;
+    [SerializeField] private float dimensionTile;
+    private Vector3 posicionOriginal;
+
+    // @NOTE: esta lista de unidades vendria desde la escena de mapa general
+    private string[] unidadesEjercito;
+
+    public void CrearEscenario()
+    {
+        // @TODO: setear los obstaculos del mapa y las zonas de despliegue
+
+        contenedorMapa = new GameObject("ContenedorDeMapa").transform;
+        InstanciarEscenario();
+
+        listaTilesDeMovimiento = new List<GameObject>();
+        contenedorTiles = new GameObject("ContenedorDeTilesDisponibles").transform;
+        contenedorTiles.SetParent(contenedorMapa);
+        
+        // setea el offset de la grilla
+        posicionOriginal = new Vector3(-dimensionTile*8, -dimensionTile*6);
+
+        contenedorUnidades = new GameObject("ContenedorDeUnidades").transform;
+        contenedorUnidades.SetParent(contenedorMapa);
+        
+        unidadesEjercito = new string[1];
+        unidadesEjercito[0] = "InfanteriaHacha";
+        InstanciarUnidades(unidadesEjercito);
+    }
+
+    private void InstanciarEscenario()
+    {
+        // @NOTE: revisar! puede que alguno de los conjuntos no lleve uno de los componentes (rios, muros, etc)
+
+        // elije (random) que conjunto de elementos se va a cargar (temp)
+        int indiceRandom = Random.Range(0, mapas.GetLength(0));
+
+        // crea el escenario con objetos random (temp)
+        InstanciarDesdeArray(mapas, indiceRandom, contenedorMapa);
+        InstanciarDesdeArray(rios, indiceRandom, contenedorMapa);
+        InstanciarDesdeArray(muros, indiceRandom, contenedorMapa);
+    }
+
+    public void InstanciarUnidades(string[] listaNombresUnidad)
+    {
+        // recorre la lista de nombres, instanciando las unidades segun el nombre
+        for (int i = 0; i < listaNombresUnidad.GetLength(0); i++)
+        {
+            int posicionTipoUnidad = -1;
+
+            if (listaNombresUnidad[i] == "InfanteriaHacha")
+                posicionTipoUnidad = 0;
+            
+            else if (listaNombresUnidad[i] == "Infanteria_1")
+                posicionTipoUnidad = 1;
+            
+            else if (listaNombresUnidad[i] == "Infanteria_2")
+                posicionTipoUnidad = 2;
+
+            try {
+                InstanciarDesdeArray(unidades, posicionTipoUnidad, contenedorUnidades);
+            }
+            // en caso de que el argumento no sea valido
+            catch {
+                Debug.LogError("No se encontro la unidad (" + listaNombresUnidad[i] + ") que se desea instanciar... Controlador Mapa -> InstanciarUnidad() : GameObject");
+            }
+        }
+    }
+
+    private void InstanciarDesdeArray(GameObject[] arrayObjetos, int indice, Transform contenedorPadre)
+    {
+        // instancia el objeto en la posicion elejida
+        GameObject objetoAInstanciar = arrayObjetos[indice];
+        GameObject instancia = Instantiate(objetoAInstanciar, objetoAInstanciar.transform.position, Quaternion.identity);
+
+        // establece al contenedor como padre del objeto
+        instancia.transform.SetParent(contenedorPadre);
+    }
+
+    public void InstanciarTilesDeMovimiento(List<Vector2> listaPosicionesTiles)
+    {
+        // Instancia los tiles disponibles en las posiciones del parametro
+
+        foreach (Vector2 posTile in listaPosicionesTiles)
+        {
+            // obtiene la posicion en el mundo del tile
+            Vector3 posTileEnMundo = ObtenerPosMundo((int)posTile.x, (int)posTile.y);
+            posTileEnMundo.x += 64;
+            posTileEnMundo.y += 64;
+            posTileEnMundo.z = tileMovimiento.transform.position.z;
+
+            GameObject instancia = Instantiate(tileMovimiento, posTileEnMundo, Quaternion.identity);
+            listaTilesDeMovimiento.Add(instancia);
+            
+            instancia.transform.SetParent(contenedorTiles);
+        }
+    }
+
+    public void DestruirTilesDeMovimiento(List<Vector2> listaPosicionesTiles)
+    {
+        foreach (GameObject tileDeMovimiento in listaTilesDeMovimiento)
+            Destroy(tileDeMovimiento);
+
+        listaPosicionesTiles.Clear();
+        listaTilesDeMovimiento.Clear();
+    }
+
+    public List<Vector2> CheckColisiones(List<Vector2> posicionesTiles)
+    {
+        List<Vector2> listaInvalidos = new List<Vector2>();
+
+        foreach (Vector2 posTile in posicionesTiles)
+        {
+            Vector3 posMundo = ObtenerCentroTile((int)posTile.x, (int)posTile.y);
+
+            // check si la posicion colisiona con algun colider (solo comprueba con objetos que esten dentro de la layer Colision)
+            if (Physics2D.OverlapPoint(posMundo, layerColision))
+                // agrega a la lista de invalidos
+                listaInvalidos.Add(posTile);
+        }
+
+        // remueve cada elemento "marcado como invalido" de la lista de disponibles
+        foreach (Vector2 posTileInvalido in listaInvalidos)
+            posicionesTiles.Remove(posTileInvalido);
+
+        return posicionesTiles;
+    }
+
+    public Vector3 ObtenerPosMundo(int x, int y)
+    {
+        return new Vector3(x, y) * dimensionTile + posicionOriginal;
+    }
+
+    public bool ObtenerPosGrilla(Vector3 posMundo, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((posMundo - posicionOriginal).x / dimensionTile);
+        y = Mathf.FloorToInt((posMundo - posicionOriginal).y / dimensionTile);
+
+        if (x >= 0 && y >= 0 && x < ancho && y < alto)
+            return true;
+
+        return false;
+    }
+
+    public Vector3 ObtenerCentroTile(int x, int y)
+    {
+        return ObtenerPosMundo(x, y) + new Vector3(dimensionTile, dimensionTile) * .5f;
+    }
+}
